ecdee82acc7c7bb4b624668ab7fc179b8682200a
Imanol Sardón <sardondelgadoimanol@gmail.com>
2019-04-06 20:05:56 +0200

    'Bateria' class junk deleted.

--- @Bateria/Bateria.m
+++ @Bateria/Bateria.m
@@ -1,27 +1,3 @@
-% ## Copyright (C) 2019 imanol
-% ## 
-% ## This program is free software; you can redistribute it and/or modify it
-% ## under the terms of the GNU General Public License as published by
-% ## the Free Software Foundation; either version 3 of the License, or
-% ## (at your option) any later version.
-% ## 
-% ## This program is distributed in the hope that it will be useful,
-% ## but WITHOUT ANY WARRANTY; without even the implied warranty of
-% ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% ## GNU General Public License for more details.
-% ## 
-% ## You should have received a copy of the GNU General Public License
-% ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
-% 
-% ## -*- texinfo -*- 
-% ## @deftypefn {Function File} {@var{retval} =} Bateria (@var{input1}, @var{input2})
-% ##
-% ## @seealso{}
-% ## @end deftypefn
-% 
-% ## Author: imanol <imanol@debian>
-% ## Created: 2019-03-26
-
 classdef Bateria
     properties
         r_c
@@ -211,8 +187,8 @@ classdef Bateria
             else
                 bateria = Bateria();
                 bateria = bateria.ajuste_coeficientes_descarga_tipo1(archivo, 1);
-                coefs = bateria.coeficientes_descarga_tipo1
-                u0 = [coefs(1), coefs(2), -1e-7, 1e-8, bateria.r_d]
+                coefs = bateria.coeficientes_descarga_tipo1;
+                u0 = [coefs(1), coefs(2), -1e-7, 1e-8, bateria.r_d];
                 obj = obj.ajuste_coeficientes_descarga(archivo, 2, u0);
             end
         end
